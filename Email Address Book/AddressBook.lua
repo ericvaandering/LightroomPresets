34fb413f1c1f25a4c5f076bb785f256bfed2c28d
Eric Vaandering <ericvaandering@gmail.com>
2013-06-23 12:10:52 -0500

    Add remainders

---
+++ Email Address Book/AddressBook.lua
@@ -0,0 +1,15 @@
+local list = {
+	{
+		address = "ccphysics@gmail.com",
+		name = "Cynthia Coutré",
+		select = false,
+		type = "Address",
+	},
+	{
+		address = "kimberlymyles@me.com",
+		name = "Kimberly Myles",
+		select = false,
+		type = "Address",
+	},
+}
+return list
