df5bc5186afc2c8b4b9725c7659484dc965e2097
Eric Vaandering <ericvaandering@gmail.com>
2013-11-21 06:55:45 -0600

    Catch up

--- Email Address Book/AddressBook.lua
+++ Email Address Book/AddressBook.lua
@@ -1,15 +1,3 @@
 local list = {
-	{
-		address = "ccphysics@gmail.com",
-		name = "Cynthia Coutré",
-		select = false,
-		type = "Address",
-	},
-	{
-		address = "kimberlymyles@me.com",
-		name = "Kimberly Myles",
-		select = false,
-		type = "Address",
-	},
 }
 return list
